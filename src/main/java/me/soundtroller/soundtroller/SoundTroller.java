2df34f3b83691c4d5346ac6666b6efe87e02d8c8
Samuel Coniglio <samuelconiglio2003@gmail.com>
2023-01-13 14:08:39 -0500

    Version 1.1:Refactored code to make main plugin function to soundtroller, added limited voucher functionality; it works with /sountroller, updated outdated functions to work natively on spigot Id>spigot-api</artifactId>, and made various other miscellaneous changes.

--- src/main/java/me/soundtroller/soundtroller/SoundTroller.java
+++ src/main/java/me/soundtroller/soundtroller/SoundTroller.java
@@ -4,16 +4,22 @@ import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.Sound;
+import org.bukkit.Bukkit;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.*;
 
-public final class SoundTroller extends JavaPlugin {
+public final class SoundTroller extends JavaPlugin implements Listener {
 
     // A list of sound aliases
     private List<String> aliases;
@@ -31,15 +37,13 @@ public final class SoundTroller extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        for (Sound sound : Sound.values()) {
-            System.out.println(sound.name());
-        }
-
         // Load the categories and rules from the config file
         loadConfig();
 
         // Save the default config file if it doesn't exist
         saveDefaultConfig();
+
+        Bukkit.getPluginManager().registerEvents(this, this);
 
         permissionMessage = ChatColor.translateAlternateColorCodes('&', "&0[&2SoundTroller&0] &cYou do not have permission to use this command.");
 
@@ -58,92 +62,161 @@ public final class SoundTroller extends JavaPlugin {
         return (ChatColor.BLACK + "[" + ChatColor.DARK_GREEN + "SoundTroller" + ChatColor.BLACK + "] " + ChatColor.RESET);
     }
 
+    @EventHandler
+    public void PlayerInteractEvent(PlayerInteractEvent event) {
+        System.out.println("line 65");
+        Player player = event.getPlayer();
+        ItemStack item = event.getPlayer().getItemInHand();
+
+        // Check if the event is a right click
+        if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return;
+        }
+
+        // Check if the item is a voucher
+        if (item == null || !item.hasItemMeta()) {
+            return;
+        }
+
+        ItemMeta meta = item.getItemMeta();
+        if (!meta.hasDisplayName()) {
+            return;
+        }
+
+        String displayName = meta.getDisplayName();
+        if (!displayName.startsWith(ChatColor.GREEN + "SoundTroller Voucher - ")) {
+            return;
+        }
+
+        // Execute the command bound to the voucher
+        String command = displayName.substring(ChatColor.GREEN.toString().length() + "SoundTroller Voucher - ".length());
+        command = "soundtroller player " + command;
+        System.out.println(command);
+        Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), command);
+        player.sendMessage(ChatColor.GREEN + "The command '" + command + "' has been executed.");
+        player.getInventory().setItemInHand(null);
+    }
+
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         try{
-            if (cmd.getName().equalsIgnoreCase("soundtroll")) {
-                if (!sender.hasPermission("soundtroll.use")) {
-                    System.out.println("Test.");
-                    sender.sendMessage(permissionMessage);
-                    return true;
-                }
-
-                // Check if the player provided a category name
-                if (args.length == 0) {
-                    sender.sendMessage(getPluginNamePrefix() + ChatColor.AQUA + "Usage: /soundtroll player <player> <alias>, /soundtroll random/all <alias> | Available Sound Aliases (Categories): " + String.join(", ", aliases));
-                    return true;
-                }
-
-                if (args[0].toLowerCase().equals("player")) {
-                    String playerName = args[1].toLowerCase();
-                    Player player = Bukkit.getPlayer(playerName);
-                    System.out.println(player.getName());
-
-                    String alias = args[2].toLowerCase();
-                    int index = aliases.indexOf(alias);
-                    List<String> aliasSounds = sounds.get(index);
-                    System.out.println(alias);
-                    System.out.println(index);
-                    System.out.println(aliasSounds);
-
-                    for (String sound : aliasSounds) {
-                        Sound soundEnumValue = Sound.valueOf(sound);
-                        Location location = player.getLocation();
-                        player.playSound(location, soundEnumValue, 1.0f, 1.0f);
-                    }
-                    sender.sendMessage(getPluginNamePrefix() + ChatColor.AQUA + "Trolled " + player.getName() + " with " + String.join(" with ", aliasSounds));
-                    return true;
-                } else if (args[0].toLowerCase().equals("all")) {
-
-                    String alias = args[1].toLowerCase();
-                    int index = aliases.indexOf(alias);
-                    List<String> aliasSounds = sounds.get(index);
-
-                    List<Player> onlinePlayers = new ArrayList<>(Bukkit.getOnlinePlayers());
-                    for (Player player : onlinePlayers) {
-                        for (String sound : aliasSounds) {
-                            Sound soundEnumValue = Sound.valueOf(sound);
-                            Location location = player.getLocation();
-                            player.playSound(location, soundEnumValue, 1.0f, 1.0f);
+            if(args.length == 0 && cmd.getName().equalsIgnoreCase("soundtroller")){
+                sender.sendMessage(getPluginNamePrefix() + ChatColor.AQUA + "Usage: /soundtroller player <player> <alias>, /soundtroller random/all <alias>, /soundtroller voucher <player> <alias> | Available Sound Aliases (Categories): " + String.join(", ", aliases));
+            }
+            if (cmd.getName().equalsIgnoreCase("soundtroller")) {
+                if ("voucher".equals(args[0])) {
+                    if (!sender.hasPermission("soundtroller.voucher")) {
+                        return true;
+                    } else if (sender.hasPermission("soundtroller.voucher")) {
+                        if (!(sender instanceof Player)) {
+                            sender.sendMessage(getPluginNamePrefix() + ChatColor.RED + "This command can only be used by players.");
+                            return true;
                         }
-                        sender.sendMessage(getPluginNamePrefix() + ChatColor.AQUA + "Trolled " + player.getName() + " with " + String.join(", ", aliasSounds));
+
+                        Player player = (Player) sender;
+
+                        // Check if the player provided a valid command to execute
+                        if (args.length == 0 || args.length == 1) {
+                            player.sendMessage(getPluginNamePrefix() + ChatColor.AQUA + "Usage: /soundtroller voucher <player> <alias>");
+                            return true;
+                        }
+
+                        StringBuilder sb = new StringBuilder();
+                        sb.append(args[1]);
+                        sb.append(" ");
+                        sb.append(args[2]);
+                        sb.append(" ");
+                        String aliasAndPlayer = sb.toString().trim();
+
+                        // Create the voucher item and add it to the player's inventory
+                        ItemStack voucher = new ItemStack(Material.PAPER);
+                        ItemMeta meta = voucher.getItemMeta();
+                        meta.setDisplayName(ChatColor.GREEN + "SoundTroller Voucher - " + aliasAndPlayer);
+                        voucher.setItemMeta(meta);
+                        player.getInventory().addItem(voucher);
+
+                        player.sendMessage(getPluginNamePrefix() + ChatColor.GREEN + "You have been given a voucher that can be used to execute a soundtroller command for the following alias and player: " + aliasAndPlayer);
+
+                        return true;
                     }
-                    return true;
-                } else if (args[0].toLowerCase().equals("random")) {
-
-                    for (Sound sound : Sound.values()) {
-                        System.out.println(sound.name());
-                    }
-
-                    String alias = args[1].toLowerCase();
-                    int index = aliases.indexOf(alias);
-                    List<String> aliasSounds = sounds.get(index);
-
-                    Random random = new Random();
-                    List<Player> onlinePlayers = new ArrayList<>(Bukkit.getOnlinePlayers());
-                    Player player = onlinePlayers.get(random.nextInt(onlinePlayers.size()));
-
-                    for (String sound : aliasSounds) {
-                        Sound soundEnumValue = Sound.valueOf(sound);
-                        Location location = player.getLocation();
-                        player.playSound(location, soundEnumValue, 1.0f, 1.0f);
-                    }
-                    sender.sendMessage(getPluginNamePrefix() + ChatColor.AQUA + "Trolled " + player.getName() + " with " + String.join(", ", aliasSounds));
-                    return true;
-                } else {
-                    sender.sendMessage(getPluginNamePrefix() + ChatColor.RED + "Invalid command. Please do /soundtroll for details on how to use a correct command.");
                 }
-                return true;
+                if ("player".equals(args[0]) || "random".equals(args[0]) || "all".equals(args[0])) {
+                    if (!sender.hasPermission("soundtroller.troll")) {
+                        sender.sendMessage(permissionMessage);
+                        return true;
+                    } else if (sender.hasPermission("soundtroller.use")) {
+
+                        // Check if the player provided a category name
+                        if (args.length == 0 || args.length == 1) {
+                            sender.sendMessage(getPluginNamePrefix() + ChatColor.AQUA + "Usage: /soundtroller player <player> <alias>, /soundtroller random/all <alias> | Available Sound Aliases (Categories): " + String.join(", ", aliases));
+                            return true;
+                        }
+
+                        if (args[0].toLowerCase().equals("player")) {
+                            String playerName = args[1].toLowerCase();
+                            Player player = Bukkit.getPlayer(playerName);
+
+                            String alias = args[2].toLowerCase();
+                            int index = aliases.indexOf(alias);
+                            List<String> aliasSounds = sounds.get(index);
+
+                            for (String sound : aliasSounds) {
+                                Sound soundEnumValue = Sound.valueOf(sound);
+                                Location location = player.getLocation();
+                                player.playSound(location, soundEnumValue, 1.0f, 1.0f);
+                            }
+                            sender.sendMessage(getPluginNamePrefix() + ChatColor.AQUA + "Trolled " + player.getName() + " with " + String.join(", and ", aliasSounds));
+                            return true;
+                        } else if (args[0].toLowerCase().equals("all")) {
+
+                            String alias = args[1].toLowerCase();
+                            int index = aliases.indexOf(alias);
+                            List<String> aliasSounds = sounds.get(index);
+
+                            List<Player> onlinePlayers = new ArrayList<>(Bukkit.getOnlinePlayers());
+                            for (Player player : onlinePlayers) {
+                                for (String sound : aliasSounds) {
+                                    Sound soundEnumValue = Sound.valueOf(sound);
+                                    Location location = player.getLocation();
+                                    player.playSound(location, soundEnumValue, 1.0f, 1.0f);
+                                }
+                                sender.sendMessage(getPluginNamePrefix() + ChatColor.AQUA + "Trolled " + player.getName() + " with " + String.join(", and ", aliasSounds));
+                            }
+                            return true;
+                        } else if (args[0].toLowerCase().equals("random")) {
+
+                            String alias = args[1].toLowerCase();
+                            int index = aliases.indexOf(alias);
+                            List<String> aliasSounds = sounds.get(index);
+
+                            Random random = new Random();
+                            List<Player> onlinePlayers = new ArrayList<>(Bukkit.getOnlinePlayers());
+                            Player player = onlinePlayers.get(random.nextInt(onlinePlayers.size()));
+
+                            for (String sound : aliasSounds) {
+                                Sound soundEnumValue = Sound.valueOf(sound);
+                                Location location = player.getLocation();
+                                player.playSound(location, soundEnumValue, 1.0f, 1.0f);
+                            }
+                            sender.sendMessage(getPluginNamePrefix() + ChatColor.AQUA + "Trolled " + player.getName() + " with " + String.join(", and ", aliasSounds));
+                            return true;
+                        } else {
+                            sender.sendMessage(getPluginNamePrefix() + ChatColor.RED + "Invalid command. Please do /soundtroll for details on how to use a correct command.");
+                        }
+                        return true;
+                    }
+                }
             }
         } catch (IllegalArgumentException illegalArgumentException) {
-            sender.sendMessage(getPluginNamePrefix() + ChatColor.RED + "A Java language IllegalArgument Exception occurred. This may be because you tried to use an alias or a sounds (collection) name that do not exist within the config.yml file for the SoundTroller plugin. Please check the console for the full stack trace.");
+            sender.sendMessage(getPluginNamePrefix() + ChatColor.RED + "A Java language IllegalArgument Exception occurred. This may be because you tried to use an alias or a sounds (aliases) name that do not exist within the config.yml file for the SoundTroller plugin. Please check the console for the full stack trace.");
             StringWriter sw = new StringWriter();
             PrintWriter pw = new PrintWriter(sw);
             illegalArgumentException.printStackTrace(pw);
             Bukkit.getServer().getConsoleSender().sendMessage(sw.toString());
             return true;
         } catch (IndexOutOfBoundsException indexOutOfBoundsException) {
-            sender.sendMessage(getPluginNamePrefix() + ChatColor.RED + "A Java language IndexOutOfBoundsException occurred. This may be because the minecraft sounds for one or more of the sound (collections) in the config.yml contains a sound that does not exist in the version bukkit that this server's main jar file is using or is based on. A full list of supported sounds for the version of spigot that this plugin was made for (.19.2-R0.1-SNAPSHOT) can be found as a comment in the config.yml file, and that list of available sounds might hold true for all instances of 1.8 or at least 1.8.8 minecraft. Please check the console for the full stack trace.");
+            sender.sendMessage(getPluginNamePrefix() + ChatColor.RED + "A Java language IndexOutOfBoundsException occurred. This may be because the minecraft sounds for one or more of the sounds (aliases) in the config.yml contains a sound that does not exist in the version bukkit that this server's main jar file is using or is based on. A full list of supported sounds for the version of spigot that this plugin was made for (.19.2-R0.1-SNAPSHOT) can be found as a comment in the config.yml file, and that list of available sounds might hold true for all instances of 1.8 or at least 1.8.8 minecraft. Please check the console for the full stack trace.");
             StringWriter sw = new StringWriter();
             PrintWriter pw = new PrintWriter(sw);
             indexOutOfBoundsException.printStackTrace(pw);
